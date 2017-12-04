61b631dabf35431ca1c732ad07f0498153e5c55e
thoriqnf <thoriqnfaizal@gmail.com>
2017-12-05 01:30:22 +0700

    editing Header

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -4,43 +4,43 @@ import React, { Component } from 'react';
 class Headers extends Component {
   render() {
     return (
-      <div className="App">
+      <div classNameName="App">
         <header id="home">
           <nav id="nav-wrap">
 
-            <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-     	      <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+     	      <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
 
-            <ul id="nav" class="nav">
-             <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
-             <li><a class="smoothscroll" href="#about">About</a></li>
- 	           <li><a class="smoothscroll" href="#resume">Resume</a></li>
-             <li><a class="smoothscroll" href="#portfolio">Works</a></li>
-             <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
-             <li><a class="smoothscroll" href="#contact">Contact</a></li>
+            <ul id="nav" className="nav">
+             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+             <li><a className="smoothscroll" href="#about">About</a></li>
+ 	           <li><a className="smoothscroll" href="#resume">Resume</a></li>
+             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+             <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
 
           </nav>
 
-           <div class="row banner">
-              <div class="banner-text">
-                 <h1 class="responsive-headline">I'm Thoriq Nur Faizal.</h1>
+           <div className="row banner">
+              <div className="banner-text">
+                 <h1 className="responsive-headline">I'm Thoriq Nur Faizal.</h1>
                  <h3>I'm a Jakarta based <span>Full stack Developer</span>, creating awesome and
-                 effective visual identities for companies of all sizes around the globe. Let's <a class="smoothscroll" href="#about">start scrolling </a>
-                 and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
+                 effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling </a>
+                 and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                  <hr />
-                 <ul class="social">
-                    <li><a href="https://www.facebook.com/thoriqnf"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/thoriqnf"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="https://www.instagram.com/thoriqnf"><i class="fa fa-instagram"></i></a></li>
-                    <li><a href="https://dribbble.com/thoriqnf"><i class="fa fa-dribbble"></i></a></li>
-                    <li><a href="https://www.github.com/thoriqnf"><i class="fa fa-github"></i></a></li>
+                 <ul className="social">
+                    <li><a href="https://www.facebook.com/thoriqnf"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/thoriqnf"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://www.instagram.com/thoriqnf"><i className="fa fa-instagram"></i></a></li>
+                    <li><a href="https://dribbble.com/thoriqnf"><i className="fa fa-dribbble"></i></a></li>
+                    <li><a href="https://www.github.com/thoriqnf"><i className="fa fa-github"></i></a></li>
                  </ul>
               </div>
            </div>
 
-           <p class="scrolldown">
-              <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+           <p className="scrolldown">
+              <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
            </p>
 
         </header>
